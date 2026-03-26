9f45c57f8645efbbb70d93a5bcad148708cc0634
David <manaevdvlad@gmail.com>
2026-03-26 20:27:55 +0300

    4 module 1,2,3 tasks

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,10 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  for (let i = 0; i < table.rows.length; i++) {
+    let row = table.rows[i];
+    let cell = row.cells[i];
+    
+    if (cell) {
+      cell.style.backgroundColor = 'red';
+    }
+  }
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,29 @@
 function highlight(table) {
-  // ваш код...
+  const tbody = table.querySelector('tbody');
+  const rows = tbody.rows;
+
+  for (let row of rows) {
+    // 1. Проверка Status (data-available)
+    const statusCell = row.cells[3];
+    if (statusCell.hasAttribute('data-available')) {
+      const isAvailable = statusCell.dataset.available === 'true';
+      row.classList.add(isAvailable ? 'available' : 'unavailable');
+    } else {
+      row.hidden = true; // Проставить атрибут hidden, если атрибута нет
+    }
+
+    // 2. Проверка Gender (m/f)
+    const genderCell = row.cells[2];
+    if (genderCell.textContent === 'm') {
+      row.classList.add('male');
+    } else if (genderCell.textContent === 'f') {
+      row.classList.add('female');
+    }
+
+    // 3. Проверка Age (< 18)
+    const ageCell = row.cells[1];
+    if (parseInt(ageCell.textContent, 10) < 18) {
+      row.style.textDecoration = 'line-through';
+    }
+  }
 }
